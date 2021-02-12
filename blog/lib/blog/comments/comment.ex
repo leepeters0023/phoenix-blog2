7a3453c0201f87c99151dd94ba35a8e41674ae58
Lee Peters <lpeters0023@gmail.com>
2021-02-12 14:16:53 -0500

    adds comments and alias

---
+++ blog/lib/blog/comments/comment.ex
@@ -0,0 +1,21 @@
+defmodule Blog.Comments.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Blog.Posts.Post
+  
+  schema "comments" do
+    field :content, :string
+    field :name, :string
+    field :post_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:name, :content])
+    |> validate_required([:name, :content])
+  end
+end
