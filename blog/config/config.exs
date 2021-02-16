de58c38f0e04aaadadf06285c0a238dc8eb6cb7a
Lee Peters <lpeters0023@gmail.com>
2021-02-16 07:20:16 -0500

    some postgres stuff I dont understand

--- blog/config/config.exs
+++ blog/config/config.exs
@@ -7,6 +7,14 @@
 # General application configuration
 use Mix.Config
 
+config :blog, Blog.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "blog_dev",
+  hostname: "localhost",
+  show_sensitive_data_on_connection_error: true,
+  pool_size: 10
+
 config :blog,
   ecto_repos: [Blog.Repo]
 
